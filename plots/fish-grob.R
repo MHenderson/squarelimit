4b1ffdf20ceb8f9a200d576c96cc3c9791a33875
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-03-18 17:28:17 +0000

    A plot with three fish.

--- plots/fish-grob.R
+++ plots/fish-grob.R
@@ -4,8 +4,15 @@ library(svgparser)
 
 fish <- read_svg(here("fish.svg"))
 
-fish_above_fish <- above(fish, fish)
+fish_above_fish_1 <- above(fish, fish)
+
+# rotation works for gTree
+fish_above_fish_2 <- editGrob(fish, vp = viewport(angle=80))
+# for translation, I think we have to move the viewport, not the grob
+fish_above_fish_3 <- editGrob(fish_above_fish_2, vp = viewport(x = fish_above_fish_2$vp$x - grid::unit(0.5, "npc")))
 
 png(here("plots", "fish-grob.png"))
-grid::grid.draw(fish_above_fish)
+grid::grid.draw(fish_above_fish_1)
+grid::grid.draw(fish_above_fish_2)
+grid::grid.draw(fish_above_fish_3)
 dev.off()
