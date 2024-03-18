e557ee031e2910b9e9762b545410b8674c42670d
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-03-18 14:37:42 +0000

    At least above works.

--- plots/fish-grob.R
+++ plots/fish-grob.R
@@ -1,8 +1,11 @@
+library(funcgeo)
 library(here)
 library(svgparser)
 
-fish_grob <- read_svg(here("fish.svg"))
+fish <- read_svg(here("fish.svg"))
+
+fish_above_fish <- above(fish, fish)
 
 png(here("plots", "fish-grob.png"))
-grid::grid.draw(fish_grob)
+grid::grid.draw(fish_above_fish)
 dev.off()
